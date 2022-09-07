9dd2e8bc0ad8ac58a8682d4966ba6c34180f531f
unknown <corona.romero.river@gmail.com>
2022-09-07 03:02:58 -0500

    actualización septiembre 2022

--- cv/assets/js/crearimg.js
+++ cv/assets/js/crearimg.js
@@ -1,8 +1,9 @@
 window.onload=function(){
+   
     var img = document.getElementById("contenedorGeneral");   
 
     var x=html2canvas(img).then(function(canvas) {
-        Canvas2Image.saveAsImage(canvas, canvas.width, canvas.heigth, ".jpeg", "cvm");
+        Canvas2Image.saveAsImage(canvas, canvas.width, canvas.heigth, "jpeg", "cvm");
         
     });
     console.log(x);
